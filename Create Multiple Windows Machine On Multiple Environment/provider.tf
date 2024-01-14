b5efd7470da954262ffa23565db77755ea7ff7c6
maran <jmaran94@gmail.com>
2024-01-14 21:41:47 +0530

    new try commit 6

--- Create Multiple Windows Machine On Multiple Environment/provider.tf
+++ Create Multiple Windows Machine On Multiple Environment/provider.tf
@@ -25,10 +25,7 @@ backend "azurerm" {
 #Configure Azure Provider
 provider "azurerm" {
 
-  subscription_id = var.az_subscription_id
-  tenant_id       = var.az_tenant_id
-  client_id       = var.az_client_id
-  client_secret   = var.az_client_secret
+  
   features {
 
   }
